e10cc88a95efb95209b0481fa969b9dd5f0f7fb6
MonicaNSTorres <monicanstorress@gmail.com>
2026-05-18 13:21:40 -0300

    inserindo ogimage

--- app/layout.tsx
+++ app/layout.tsx
@@ -63,7 +63,7 @@ export const metadata: Metadata = {
     type: "website",
     images: [
       {
-        url: "/og-image.jpg",
+        url: "/og-image.jpeg",
         width: 1200,
         height: 630,
         alt: "Dra. Tauana Brógio Leão - Endocrinologista em São José dos Campos",
